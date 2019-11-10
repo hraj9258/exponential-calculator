e8974aafd756dfcac2aef516dd7c196d987c051d
Himanshu Raj <himanshurajkush@gmail.com>
2019-11-10 13:19:56 +0530

    just having fun

--- exponential-calculator.py
+++ exponential-calculator.py
@@ -2,4 +2,4 @@ x=int(input("Please enter the Number: "))
 y=int(input("Please enter the exponetial number: "))
 print("Value of the given exponential number",x, end=" ")
 print("is",x**y)
-#just added a new line
+#just added a new line.
